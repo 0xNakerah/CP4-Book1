4594ed11d6a66c2023ffda54b9951d52e321e16a
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 11:00:19 +0200

    Create UVa 10071.cpp

---
+++ 1-Introduction/1.4-Getting Started/a. Sequences Only/UVa 10071.cpp
@@ -0,0 +1,21 @@
+//problem link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=24&page=show_problem&problem=1012
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+
+void solve(){
+	int v,t;
+	while(cin >> v >> t){
+		cout << v*2*t << '\n';
+	}
+}
+int main(){
+  ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

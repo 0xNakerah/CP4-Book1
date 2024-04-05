bbac99d13e27a1a94ff34498255690672ed86649
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 12:35:56 +0200

    Create Kattis - isithalloween.cpp

---
+++ 1-Introduction/1.4-Getting Started/c. Selection Only/Kattis - isithalloween.cpp
@@ -0,0 +1,21 @@
+//problem link: https://open.kattis.com/contests/zbitrk/problems/isithalloween
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+
+void solve(){
+	string month; int day;
+	cin >> month >> day;
+	cout << ((month=="OCT"&&day==31)||(month=="DEC"&&day==26)?"yup":"nope") << '\n';
+}
+
+int main(){
+  ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

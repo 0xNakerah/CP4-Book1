b5160e55f1ffeeb66047d3a0a30cdbecc1c89f34
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 16:01:30 +0200

    Update Kattis - isithalloween.cpp

--- 1-Introduction/1.4-Getting Started/c. Selection Only/Kattis - isithalloween.cpp
+++ 1-Introduction/1.4-Getting Started/c. Selection Only/Kattis - isithalloween.cpp
@@ -6,16 +6,16 @@ using namespace std;
 #define PB push_back
 
 void solve(){
-	string month; int day;
-	cin >> month >> day;
-	cout << ((month=="OCT"&&day==31)||(month=="DEC"&&day==26)?"yup":"nope") << '\n';
+    string month; int day;
+    cin >> month >> day;
+    cout << ((month=="OCT"&&day==31)||(month=="DEC"&&day==25)?"yup":"nope") << '\n';
 }
 
 int main(){
   ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
-	int testcase = 1;
-	//cin >> testcase;
-	while(testcase--){
-		solve();
-	}
-} 	 		   			  	
+    int testcase = 1;
+    //cin >> testcase;
+    while(testcase--){
+        solve();
+    }
+}              

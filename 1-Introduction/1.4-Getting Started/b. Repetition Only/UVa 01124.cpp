4a1b75a9e37a2343c43d2a81981bae83045f64f6
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 11:42:07 +0200

    Create UVa 01124.cpp

---
+++ 1-Introduction/1.4-Getting Started/b. Repetition Only/UVa 01124.cpp
@@ -0,0 +1,22 @@
+//problem link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=3565
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+
+void solve(){
+	string s;
+	while(getline(cin,s)){
+		cout << s << '\n';
+	}
+}
+
+int main(){
+   	ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

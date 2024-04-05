585ae862a475116f54c60c97fe1a79f83c662405
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 11:25:45 +0200

    Create Kattis - carrots.cpp

---
+++ 1-Introduction/1.4-Getting Started/a. Sequences Only/Kattis - carrots.cpp
@@ -0,0 +1,21 @@
+//problem link: https://open.kattis.com/contests/ccpc18-practice/problems/carrots
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+
+void solve(){
+	int n,p; cin >> n >> p;
+	string trash; cin >> trash;
+	cout << p << '\n';
+}
+
+int main(){
+  ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

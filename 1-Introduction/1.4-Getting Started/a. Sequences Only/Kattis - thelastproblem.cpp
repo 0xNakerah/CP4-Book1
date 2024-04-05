a3c18514db6ffbc3ae0f1d969d6a15ccfd918340
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 11:33:51 +0200

    Create Kattis - thelastproblem.cpp

---
+++ 1-Introduction/1.4-Getting Started/a. Sequences Only/Kattis - thelastproblem.cpp
@@ -0,0 +1,20 @@
+//problem link: https://open.kattis.com/contests/fvp5f9/problems/thelastproblem
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+
+void solve(){
+	string s; getline(cin,s);
+	cout << "Thank you, " << s << ", and farewell!\n";
+}
+
+int main(){
+  ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

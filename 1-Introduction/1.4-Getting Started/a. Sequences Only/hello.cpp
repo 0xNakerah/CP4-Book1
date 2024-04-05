506725608a6dbac379e3df0520692f2c0559a66b
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-05 10:54:33 +0200

    Create hello.cpp

---
+++ 1-Introduction/1.4-Getting Started/a. Sequences Only/hello.cpp
@@ -0,0 +1,18 @@
+//problem link: https://open.kattis.com/problems/hello
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+
+void solve(){
+	cout << "Hello World!\n";
+}
+int main(){
+   	ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

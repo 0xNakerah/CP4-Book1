5417416ff9ae5e22435493e9e36f036df15c2da2
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-06 22:29:00 +0200

    Create Kattis - bossbattle.cpp

---
+++ 1-Introduction/1.4-Getting Started/i. Still Easy/Kattis - bossbattle.cpp
@@ -0,0 +1,25 @@
+//problem link: https://open.kattis.com/problems/bossbattle
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+#define F first
+#define S second
+
+void solve(){
+	int n; cin >> n;
+	if(n<=3)cout<<1<<'\n';
+	else cout << n-2 << '\n';
+}
+
+int main(){
+  ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	//cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	

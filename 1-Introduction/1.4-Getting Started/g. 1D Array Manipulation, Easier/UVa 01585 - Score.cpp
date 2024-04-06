6fcb46abe275d32f3b1cda869560dada9872fd5f
0xNakerah <165704675+0xNakerah@users.noreply.github.com>
2024-04-06 11:04:02 +0200

    Create UVa 01585 - Score.cpp

---
+++ 1-Introduction/1.4-Getting Started/g. 1D Array Manipulation, Easier/UVa 01585 - Score.cpp
@@ -0,0 +1,28 @@
+//problem link: https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=4460
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define PB push_back
+#define sz(x) (int)x.size()
+#define all(x) x.begin(),x.end()
+
+void solve(){
+	string s; cin >> s;
+	int cnt=0,ans=0;
+	for(int i = 0; i < sz(s); i++){
+		if(s[i]=='O')cnt++;
+		else cnt = 0;
+		ans += cnt;
+	}
+	cout << ans << '\n';
+}
+
+int main(){
+  ios_base::sync_with_stdio(false), cout.tie(NULL), cin.tie(NULL);
+	int testcase = 1;
+	cin >> testcase;
+	while(testcase--){
+		solve();
+	}
+} 	 		   			  	
